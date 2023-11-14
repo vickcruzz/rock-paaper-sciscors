9d875d9d3108020be701a5012c492527ca202bff
Vick Cruzz <vickcruzz21@gamil.com>
2023-11-14 10:22:44 +0300

    first commit

---
+++ rock.js
@@ -0,0 +1,48 @@
+const playerText = document.querySelector("#playerText");
+const computerText = document.querySelector("#computerText");
+const resultText = document.querySelector("#resultText");
+const choiceBtns = document.querySelectorAll(".choiceBtn");
+let player;
+let computer;
+let result;
+
+choiceBtns.forEach((button) =>
+  button.addEventListener("click", () => {
+    player = button.textContent;
+    computerTurn();
+ 
+  playerText.textContent= `player: ${player}`
+  computerText.textContent= `computer: ${computer}`
+  resultText.textContent= checkWinner()
+}));
+function computerTurn() {
+  const randNum = Math.floor(Math.random() * 3) + 1;
+  switch (randNum) {
+    case 1:
+      computer = "ROCK";
+      break;
+    case 2:
+      computer = "PAPER";
+      break;
+    case 3:
+      computer = "SCICORS";
+      break;
+  }
+}
+function checkWinner(){
+    if (player == computer) {
+        return "Draw!"        
+    }
+
+else if(computer == "ROCK"){
+    return( player == "PAPER")? "You Win!" : "You Loose!"
+}
+
+else if(computer == "PAPER"){
+    return( player == "SCICORS")? "You Win!" : "You Loose!"
+}
+
+else if(computer == "SCICORS"){
+    return( player == "ROCK")? "You Win!" : "You Loose!"
+}
+}
